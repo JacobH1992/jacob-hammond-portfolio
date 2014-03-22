f2eb778f2d2bf11637f676e70818e5bbbd9c8e35
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-22 23:40:38 +0000

    Added inital files

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+<meta name="description" content=""/>
+<meta name="author" content="Jacob Hammond"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+<title>Jacob Hammond | Multimedia Designer</title>
+
+<link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+<!--[if IE]>
+      <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<![endif]-->
+
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="/img/favicon.ico"/>
+<link rel="apple-touch-icon-precomposed" href="/img/touchicon-57.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/img/touchicon-72.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/img/touchicon-114.png">
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/img/touchicon-144.png">
