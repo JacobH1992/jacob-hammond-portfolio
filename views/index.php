fedde12a5b930ef33c88fbd9156d32e8270a3d87
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-23 15:40:12 +0000

    Fixed vertical rhythm on typography

--- views/index.php
+++ views/index.php
@@ -1,4 +1,6 @@
-<h1>Heading One</h1>
-<h2>Heading Two</h2>
+<h2>MY NAME IS</h2>
+<h1>JACOB HAMMOND</h1>
+<h2>I AM A MULTIMEDIA DESIGNER</h2>
 <h3>Heading Three</h3>
 <a href="#">Link 1</a>
+<p>I love food, socialising, digital art, music, have a passion for design and people call me Jake. I started designing when I was 11 and haven't looked back since. Even my love for gaming had to take 2nd place. Now I am in the 4th and final year of my degree having worked as a designer for The Walt Disney Company in London for the past year.</p>
