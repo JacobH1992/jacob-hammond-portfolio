72b5e6f5b76f53988a5e9eb7613f3d3c5159ece1
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-23 01:09:54 +0000

    Added initial typography styles

--- includes/head.php
+++ includes/head.php
@@ -3,7 +3,8 @@
 <meta name="author" content="Jacob Hammond"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 <title>Jacob Hammond | Multimedia Designer</title>
-<link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<link href="/css/styles.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
 <!--[if IE]>
       <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
 <![endif]-->
--- views/index.php
+++ views/index.php
@@ -1,1 +1,4 @@
-<h1>Index</h1>
+<h1>Heading One</h1>
+<h2>Heading Two</h2>
+<h3>Heading Three</h3>
+<a href="#">Link 1</a>
