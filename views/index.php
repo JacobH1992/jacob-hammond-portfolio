9fd6f640c5a02f24669ee7ef9c07dfa94dfee57e
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-03-29 04:38:53 +0000

    Added about me content, btn styles, icons

--- views/index.php
+++ views/index.php
@@ -1,7 +1,8 @@
 <header id="index-header" class="blue">
     <div id="index-header-text" class="align-center section-padding">
-        <h1>Welcome</h1>
-        <h2>My name is Jacob Hammond, I am passionate about beautiful, clean and intuitive design.</h2>
+        <h1>Hi I'm Jacob.</h1>
+        <h2>I am passionate about beautiful, clean and intuitive design.</h2>
+        <div class="btn"><i class="ico-arrow-down"></i>A BIT MORE ABOUT ME</div>
     </div>
 </header>
 
@@ -10,27 +11,27 @@
     <section id="index-about" class="white">
         <div class="align-center section-padding">
             <h1>About</h1>
-            <h2>I am a recent graduate from the University of Kent in Canterbury, England with a degree in Multimedia Technology & Design.</h2>
+            <h2>I am a recent graduate of the University of Kent in Canterbury, England with a degree in Multimedia Technology & Design.</h2>
 <!--             <p class="half-margin">I love food, socialising, digital art, music, have a passion for design and people call me Jake. I started designing when I was 11 and haven't looked back since.</p>
-            <p class="half-margin">I have just completed the 4th and final year of my degree having worked as a freelance designer and designer for The Walt Disney Company during that time.</p> -->
+            <p class="half-margin">I have just completed the 4th and final year of my degree having worked as a freelance designer and designer for The Walt Disney Company during that time.</p> -->  
+            <p class="half-margin">I have just completed the 4th and final year of my degree. During that time I worked on many projects for my course, as a freelance designer and as Design & Production Intern for The Walt Disney Company in London.</p>
+            <p class="half-margin">My passion for design continues to grow along with my skills in many areas of design and development. As a recent graduate I am keen to find opportunities that will help further my skill set and career.</p>
         </div>
     </section>
 
     <section class="yellow">
         <div class="align-center section-padding">
-            <div class="list third-margin">
-                <h2><i class="ico-skills"></i>Skills</h2>
-                <ul>
-                    <li><p>Website Design & Development</p></li>
-                    <li><p>Graphic Design</p></li>
-                    <li><p>Digital Art</p></li>
-                    <li><p>3D Design & Animation</p></li>
-                    <li><p>UI & Mobile App Design</p></li>
-                    <li><p>Photography</p></li>
-                </ul>
+            <div class="third-margin">
+                <h3><i class="ico-keyboard"></i>Website Design</h3>
+                <h3><i class="ico-graphic"></i>Graphic Design</h3>
+                <h3><i class="ico-art"></i>Digital Art</h3>
+                <h3><i class="ico-cube"></i>3D Modelling & Animation</h3>
+                <h3><i class="ico-mobile"></i>UI & Mobile App Design</h3>
+                <h3><i class="ico-camera"></i>Photography</h3>
+                <h3><i class="ico-producer"></i>Website Production</h3>
             </div>
             <div class="list third-margin">
-                <h2><i class="ico-tools"></i>Tools</h2>
+                <h3><i class="ico-tools"></i>Tools</h3>
                 <ul>
                     <li><p>Adobe Master Collection CS6</p></li>
                     <li><p>Sublime Text 3</p></li>
@@ -45,7 +46,7 @@
                 </ul>
             </div>
             <div class="third-margin">
-                <h2><i class="ico-languages"></i>Languages</h2>
+                <h3><i class="ico-languages"></i>Languages</h3>
                 <ul>
                     <li><p>HTML5</p></li>
                     <li><p>CSS3</p></li>
