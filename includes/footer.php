43c8be269a41a033a6aa6c302347b2b049dfc333
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-04-03 21:09:30 +0100

    Updated icon font, removed Gist font, deleted images, updated styles

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,18 @@
 <footer class="blue">
     <div class="align-center section-padding">
-        <h1>
+        <h2 class="half-margin">
             <a href="/"><i class="ico-linkedin"></i></a>
             <a href="/"><i class="ico-facebook"></i></a>
             <a href="/"><i class="ico-twitter"></i></a>
             <a href="/"><i class="ico-tumblr"></i></a>
             <a href="/"><i class="ico-github"></i></a>
             <a href="/"><i class="ico-lastfm"></i></a>
-        </h1>
+        </h2>
+        <div class="half-margin">
+            <h3>Latest tweet</h3>
+            <a class="twitter-timeline" href="https://twitter.com/jacobh1992" data-width="100%" data-widget-id="278189386573676544" data-tweet-limit="1" data-chrome="noheader">Tweets by @jacobh1992</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
+        <p class="copyright full">Copyright © Jacob Hammond 2014, All Rights Reserved</p>
     </div>
 </footer>
--- views/index.php
+++ views/index.php
@@ -1,32 +1,34 @@
 <header id="index-header" class="blue">
     <div id="index-header-text" class="align-center section-padding">
-        <h1>Hi I'm Jacob.</h1>
-        <h2>I am passionate about beautiful, clean and intuitive design.</h2>
-        <div class="btn"><i class="ico-arrow-down"></i>A BIT MORE ABOUT ME</div>
+        <h1>Hi, I'm Jacob.</h1>
+        <h2>I <i class="ico-heart ico--no-margin"></i> beautiful, clean and intuitive design.</h2>
+        <a href="#about" class="btn"><i class="ico-arrow-down"></i>A BIT MORE ABOUT ME</a>
     </div>
 </header>
 
 <main>
-
     <section id="index-about" class="white">
         <div class="align-center section-padding">
             <h1>About</h1>
-            <h2>I am a recent graduate of the University of Kent in Canterbury, England with a degree in Multimedia Technology & Design.</h2>
-<!--             <p class="half-margin">I love food, socialising, digital art, music, have a passion for design and people call me Jake. I started designing when I was 11 and haven't looked back since.</p>
-            <p class="half-margin">I have just completed the 4th and final year of my degree having worked as a freelance designer and designer for The Walt Disney Company during that time.</p> -->  
+            <h2>I'm a recent <i class="ico-graduate ico--no-margin"></i> of the University of Kent in Canterbury, England with a degree in Multimedia Technology & Design.</h2>
             <p class="half-margin">I have just completed the 4th and final year of my degree. During that time I worked on many projects for my course, as a freelance designer and as Design & Production Intern for The Walt Disney Company in London.</p>
             <p class="half-margin">My passion for design continues to grow along with my skills in many areas of design and development. As a recent graduate I am keen to find opportunities that will help further my skill set and career.</p>
+            <div class="section-content-wrapper">
+                <div class="btn"><i class="ico-download"></i>DOWNLOAD MY CV</div>
+            </div>
         </div>
     </section>
 
     <section class="yellow">
         <div class="align-center section-padding">
-            <div class="third-margin">
+            <h1>Skills</h1>
+            <h2>I have acquired a wide range of skills in many different types of design.</h2>
+            <div class="list third-margin">
                 <h3><i class="ico-keyboard"></i>Website Design</h3>
                 <h3><i class="ico-graphic"></i>Graphic Design</h3>
                 <h3><i class="ico-art"></i>Digital Art</h3>
-                <h3><i class="ico-cube"></i>3D Modelling & Animation</h3>
-                <h3><i class="ico-mobile"></i>UI & Mobile App Design</h3>
+                <h3><i class="ico-cube"></i>3D Design</h3>
+                <h3><i class="ico-mobile"></i>Mobile App Design</h3>
                 <h3><i class="ico-camera"></i>Photography</h3>
                 <h3><i class="ico-producer"></i>Website Production</h3>
             </div>
@@ -34,47 +36,35 @@
                 <h3><i class="ico-tools"></i>Tools</h3>
                 <ul>
                     <li><p>Adobe Master Collection CS6</p></li>
-                    <li><p>Sublime Text 3</p></li>
+                    <li><p>Sublime Text</p></li>
                     <li><p>Git / GitHub</p></li>
                     <li><p>Autodesk 3DS Max</p></li>
                     <li><p>Microsoft Visual Studio</p></li>
                     <li><p>Eclipse</p></li>
                     <li><p>Wordpress</p></li>
-                    <li><p>Microsoft Office</p></li>
-                    <li><p>FileZilla</p></li>
                     <li><p>Nikon DSLR</p></li>
                 </ul>
             </div>
-            <div class="third-margin">
+            <div class="list third-margin">
                 <h3><i class="ico-languages"></i>Languages</h3>
                 <ul>
                     <li><p>HTML5</p></li>
                     <li><p>CSS3</p></li>
-                    <li><p>Javascript &amp; Jquery</p></li>
+                    <li><p>Javascript &amp; jQuery</p></li>
                     <li><p>SASS / Compass</p></li>
                     <li><p>PHP</p></li>
                     <li><p>ActionScript 3.0</p></li>
                     <li><p>Java &amp; Android SDK</p></li>
+                    <li><p>XML, XSLT, XML Schema</p></li>
                 </ul>
             </div>
         </div>
     </section>
 
     <section class="white">
-        <div class="align-center section-padding">
-            <h1>Recent Projects</h1>
-            <div class="third-margin">
-                <img src="http://jacobhammond.co.uk/images/projects/mobileapp.jpg" alt="">
-                <p>Comfortably Stylish Mobile App</p>
-            </div>
-            <div class="third-margin">
-                <img src="http://jacobhammond.co.uk/images/projects/mosquitoanimation.jpg" alt="">
-                <p>Mosquito Animation</p>
-            </div>
-            <div class="third-margin">
-                <img src="http://jacobhammond.co.uk/images/projects/precisepixels.jpg" alt="">
-                <p>Precise Pixels V2</p>
-            </div>
+        <div id="index-work" class="align-center section-padding">
+            <h1>Recent work</h1>
+            <div class="btn"><i class="ico-arrow-left"></i>VIEW ALL MY WORK</div>
         </div>
     </section>
 
