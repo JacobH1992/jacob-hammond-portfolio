7f225d04214f6f047a3aa04513b7703bd9a7ff68
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-04-10 17:47:25 +0100

    Updated details page, added images for 3d pages

--- views/details.php
+++ views/details.php
@@ -1,34 +1,55 @@
 <header class="blue">
-    <div class="details-header align-center section-padding">
-        <div class="half-margin">
+    <div class="align-center section-padding">
             <h1><?= $title; ?></h1>
             <h2><?= $type; ?></h2>
-        </div>
-        <div class="half-margin">
-            <p><?= $description; ?></p>
-        </div>
     </div>
 </header>
 
-<main>
+<main id="details-main">
 
     <section class="white">
         <div class="align-center section-padding">
             <?= $video; ?>
-            <img src="<?= $imgLarge1; ?>" alt="<?= $title; ?>" title="<?= $title; ?>" class="description-image"/>
-            
-<!--             <div class="half-margin">
-                <img src="img/blank.gif" data-src="<?= $imgLarge2; ?>" alt="<?= $title; ?>" title="<?= $imgLarge2; ?>" class="lazy"/>
-                <noscript><img src="<?= $imgLarge2; ?>" alt="<?= $title; ?>" title="<?= $title; ?>"/></noscript>
-            </div> -->
-            
-
+            <div class="half-margin">
+                <h3><i class="ico-info"></i>About</h3>
+                <?= $description; ?>
+                <h3>Technologies used:</h3>
+                <ul>
+                    <li>WebGL</li>
+                    <li>ThreeJS</li>
+                    <li>WebRTC webcam gestures</li>
+                    <li>Google Elevation API</li>
+                    <li>Google Static Maps API</li>
+                    <li>PHP MVC</li>
+                    <li>Login system</li>
+                    <li>Checkbox hack</li>
+                    <li>Feature detection</li>
+                    <li>Font icons</li>
+                    <li>Mobile first</li>
+                    <li>Responsive</li>
+                    <li>Sass</li>
+                </ul>
+            </div>   
+            <div class="half-margin">
+                <img src="<?= $imgLarge1; ?>" alt="<?= $title; ?>" title="<?= $title; ?>" class="description-image"/>
+                <div class="details-link">
+                    <a href="htt://cell-industries.co.uk/">cell-industries.co.uk</a>
+                </div>
+            </div>
         </div>
     </section>
+            <div class="details-description section-content-wrapper">
+                <div class="half-margin">
+                    <h3>Technologies used:</h3>
+                </div>
+            </div>
 
     <section class="yellow">
-        <div class="align-center section-padding">
+        <div class="align-center section-padding">    
+
         </div>
     </section>
+
+
 
 </main>
