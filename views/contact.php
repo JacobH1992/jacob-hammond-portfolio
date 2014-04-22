ec0c80ce86d31244bc926882845f3b26676ae7cf
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-04-22 19:02:55 +0100

    Added contact cv link, changed got to have, reduced index header height

--- views/contact.php
+++ views/contact.php
@@ -1,7 +1,7 @@
 <header class="blue">
     <div class="align-center section-padding wow bounceInDown">
         <h1>Contact</h1>
-        <h2>Want to work with me? Got a question? Drop me an email:</h2>
+        <h2>Want to work with me? Have a question? Drop me an email:</h2>
         <h3><a href="mailto:contact@jacobhammond.co.uk">contact@jacobhammond.co.uk</a></h3>
     </div>
 </header>
@@ -11,7 +11,7 @@
     <section class="white">
         <div class="align-center section-padding wow slideInLeft">
             <h2>As a recent graduate I am keen to find opportunities that will help further my skill set and career.</h2>
-            <div class="btn"><i class="ico-download"></i>DOWNLOAD MY CV</div>
+            <a href="/downloads/Jacob-Hammond-Multimedia-Designer-CV-2014.pdf" target="_blank" class="btn wow fadeIn" data-wow-delay=".25s"><i class="ico-download"></i>DOWNLOAD MY CV</a>
         </div>
     </section>
 
